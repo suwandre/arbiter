fba5b57f820ecfe6985ab00bad3779faa02fc34e
suwandre <suwandresukijat@gmail.com>
2026-02-21 12:27:37 +0100

    feat: added base exchange trait

---
+++ src/exchanges/mod.rs
@@ -0,0 +1,13 @@
+use async_trait::async_trait;
+use crate::errors::ExchangeError;
+use crate::models::FundingRate;
+
+#[async_trait]
+pub trait Exchange: Send + Sync {
+    fn name(&self) -> &'static str;
+
+    async fn fetch_funding_rate(
+        &self,
+        pair: &str,
+    ) -> Result<FundingRate, ExchangeError>;
+}
