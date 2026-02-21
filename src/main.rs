9e9a185f5832c89fc45687a79df3b014d7223eb1
suwandre <suwandresukijat@gmail.com>
2026-02-21 12:06:37 +0100

    rf: added tracing subscriber to main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt()
+        .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+        .init();
+
+    tracing::info!("Arbiter starting...");
 }
