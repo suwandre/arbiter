3df7678595d4152c17dae5a4bb4e913246e1f5e4
suwandre <suwandresukijat@gmail.com>
2026-02-24 19:02:20 +0100

    feat: exchange-level structs - spread, orderbook and funding

---
+++ internal/models/models.go
@@ -0,0 +1,35 @@
+package models
+
+import "time"
+
+type FundingRate struct {
+	Exchange    string    `json:"exchange"`
+	Pair        string    `json:"pair"`
+	Rate        float64   `json:"rate"`
+	NextFunding time.Time `json:"next_funding"`
+}
+
+type Spread struct {
+	Exchange string  `json:"exchange"`
+	Pair     string  `json:"pair"`
+	Bid      float64 `json:"bid"`
+	Ask      float64 `json:"ask"`
+	Spread   float64 `json:"spread"` // Ask - Bid
+}
+
+type OrderBookDepth struct {
+	Exchange string  `json:"exchange"`
+	Pair     string  `json:"pair"`
+	BidDepth float64 `json:"bid_depth"` // total liquidity on buy side
+	AskDepth float64 `json:"ask_depth"` // total liquidity on sell side
+}
+
+type ExchangeScore struct {
+	Exchange       string    `json:"exchange"`
+	Pair           string    `json:"pair"`
+	FundingRate    float64   `json:"funding_rate"`
+	SpreadPct      float64   `json:"spread_pct"`
+	DepthScore     float64   `json:"depth_score"`
+	CompositeScore float64   `json:"composite_score"` // final ranking score
+	UpdatedAt      time.Time `json:"updated_at"`
+}
